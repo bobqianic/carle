6cdb9851c17d53f653f4cc426603c1357cc04245
bobqianic <129547291+bobqianic@users.noreply.github.com>
2025-04-06 21:47:53 +0100

    numpy 2.2.4->2.1.3 for better compatibility

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name="carle",\
         packages=["carle", "tests", "evaluation"],\
-        install_requires=["numpy==2.2.4",\
+        install_requires=["numpy==2.1.3",\
                         "torch==2.6.0",\
                         "scikit-image==0.25.2",\
                         "matplotlib==3.10.1"],\
